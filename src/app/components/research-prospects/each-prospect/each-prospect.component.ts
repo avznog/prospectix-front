0795ab12817931fdf43efa6d3743c6399a4e09d4
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-07-08 16:01:11 +0200

    ✨ Added phone edition

--- src/app/components/research-prospects/each-prospect/each-prospect.component.ts
+++ src/app/components/research-prospects/each-prospect/each-prospect.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Prospect } from 'src/app/models/prospect.model';
+import { PhonesService } from 'src/app/services/phones/phones.service';
 
 @Component({
   selector: 'app-each-prospect',
@@ -8,9 +11,21 @@ import { Prospect } from 'src/app/models/prospect.model';
 })
 export class EachProspectComponent implements OnInit {
   @Input() prospect!: Prospect;
-  constructor() { }
+  changeNumberForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private phonesService: PhonesService
+  ) { }
 
   ngOnInit(): void {
+    this.changeNumberForm = this.formBuilder.group({
+      number: ["", Validators.required]
+    })
   }
 
+  onChangePhoneNumber() : Subscription {
+    console.log(this.changeNumberForm.value);
+    console.log(this.prospect.phone.id)
+    return this.phonesService.updatePhoneNumber(this.prospect.phone.id, { number: this.changeNumberForm.value["number"]})
+  }
 }
