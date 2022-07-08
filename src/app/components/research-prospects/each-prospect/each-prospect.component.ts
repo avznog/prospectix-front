915312377409728b4de8307964345ab8919e4bd1
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-07-08 16:26:22 +0200

    ✨ website / mail edition on front

--- src/app/components/research-prospects/each-prospect/each-prospect.component.ts
+++ src/app/components/research-prospects/each-prospect/each-prospect.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Prospect } from 'src/app/models/prospect.model';
+import { EmailsService } from 'src/app/services/emails/emails.service';
 import { PhonesService } from 'src/app/services/phones/phones.service';
+import { WebsitesService } from 'src/app/services/websites/websites.service';
 
 @Component({
   selector: 'app-each-prospect',
@@ -12,20 +14,42 @@ import { PhonesService } from 'src/app/services/phones/phones.service';
 export class EachProspectComponent implements OnInit {
   @Input() prospect!: Prospect;
   changeNumberForm!: FormGroup;
+  changeEmailForm!: FormGroup;
+  changeWebsiteForm!: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
-    private phonesService: PhonesService
+    private phonesService: PhonesService,
+    private websitesService: WebsitesService,
+    private emailsService: EmailsService
+    
   ) { }
 
   ngOnInit(): void {
     this.changeNumberForm = this.formBuilder.group({
       number: ["", Validators.required]
+    });
+
+    this.changeEmailForm = this.formBuilder.group({
+      email: ["", Validators.required]
+    });
+
+    this.changeWebsiteForm = this.formBuilder.group({
+      website: ["", Validators.required]
     })
   }
 
   onChangePhoneNumber() : Subscription {
-    console.log(this.changeNumberForm.value);
-    console.log(this.prospect.phone.id)
-    return this.phonesService.updatePhoneNumber(this.prospect.phone.id, { number: this.changeNumberForm.value["number"]})
+    console.log("phone changed")
+    return this.phonesService.updatePhoneNumber(this.prospect.phone.id, { number: this.changeNumberForm.value["number"]});
+  }
+
+  onChangeEmail() : Subscription {
+    console.log("email changed")
+    return this.emailsService.updateEmail(this.prospect.email.id, { email: this.changeEmailForm.value["email"] });
+  }
+
+  onChangeWebsite() : Subscription {
+    console.log("website changed")
+    return this.websitesService.updateWebsite(this.prospect.website.id, { website: this.changeWebsiteForm.value["website"] });
   }
 }
