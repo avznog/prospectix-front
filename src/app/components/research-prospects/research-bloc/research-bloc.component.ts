981839fdf480f6f84bf3bde703b031494dde63fd
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-07-08 19:30:26 +0200

    ✨ Linked cities and activities with db

--- src/app/components/research-prospects/research-bloc/research-bloc.component.ts
+++ src/app/components/research-prospects/research-bloc/research-bloc.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs/internal/Observable';
+import { Activity } from 'src/app/models/activity.model';
+import { City } from 'src/app/models/city.model';
+import { ActivitiesService } from 'src/app/services/activities/activities.service';
+import { CitiesService } from 'src/app/services/cities/cities.service';
 
 @Component({
   selector: 'app-research-bloc',
@@ -7,9 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResearchBlocComponent implements OnInit {
 
-  constructor() { }
+  activites!: Activity[];
+  cities!: City[];
+  constructor(
+    private formBuilder: FormBuilder,
+    private readonly activitiesService: ActivitiesService,
+    private readonly citiesServices: CitiesService
+  ) { }
 
   ngOnInit(): void {
+    this.activitiesService.findAll()
+      .subscribe({
+        next: (data) => {
+          this.activites = data
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      });
+
+    this.citiesServices.findAll()
+      .subscribe({
+        next: (data) => {
+          this.cities = data;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
   }
 
 }
