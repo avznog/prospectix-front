5f4821bbb90679b6c18546b9231d0da37f1afcdc
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-07-10 13:33:18 +0200

    🚀 ✨ Implement dashboardPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { EachGoalComponent } from './components/dashboard/each-goal/each-goal.component';
+import { ListeGoalsComponent } from './components/dashboard/liste-goals/liste-goals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EachGoalComponent,
+    ListeGoalsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/dashboard/liste-goals/liste-goals.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListeGoalsComponent } from './liste-goals.component';
+
+describe('ListeGoalsComponent', () => {
+  let component: ListeGoalsComponent;
+  let fixture: ComponentFixture<ListeGoalsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListeGoalsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListeGoalsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
