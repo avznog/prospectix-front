3e087234a088d5acd780ccca906aec97b3911479
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-07-21 09:08:13 +0200

    🚚 Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
 import { EditGoalComponent } from './components/goals/edit-goal/edit-goal.component';
+import { GoalsComponent } from './components/goals/goals/goals.component';
+import { MeetingsComponent } from './components/meetings/meetings/meetings.component';
+import { RemindersComponent } from './components/reminders/reminders/reminders.component';
+import { ResearchProspectsComponent } from './components/research-prospects/research-prospects/research-prospects.component';
+import { UsersComponent } from './components/users/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HeaderComponent },
+  { path:"users", component: UsersComponent },
+  { path:"reminders", component: RemindersComponent },
+  { path:"meetings", component: MeetingsComponent },
+  { path:"prospects", component: ResearchProspectsComponent },
+  { path:"goals", component: GoalsComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
