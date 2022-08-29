1a51a86ed25c3df672e10876d7c60c8b083430f7
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-08-29 17:32:22 +0200

    🚧 changelogs working

--- src/app/services/changelogs/changelogs.service.ts
+++ src/app/services/changelogs/changelogs.service.ts
@@ -9,6 +9,58 @@ export class ChangelogsService {
   constructor() {
     this.changelogs = [
       {
+        version: 9.1,
+        date: new Date("2022-08-29T00:00:00.000Z"),
+        bugs: [
+          "Correction de l'affichage déformé du header",
+          "Correction du flash lors du changement de prospect"
+        ],
+        features: [
+          "Feature de dashboard",
+          "Feature de mail",
+          "Feature de changelog"
+        ]
+      },
+      {
+        version: 0.2,
+        date: new Date(),
+        bugs: [
+          "Correction de l'affichage déformé du header",
+          "Correction du flash lors du changement de prospect"
+        ],
+        features: [
+          "Feature de dashboard",
+          "Feature de mail",
+          "Feature de changelog"
+        ]
+      },
+      {
+        version: 0.1,
+        date: new Date(),
+        bugs: [
+          "Correction de l'affichage déformé du header",
+          "Correction du flash lors du changement de prospect"
+        ],
+        features: [
+          "Feature de dashboard",
+          "Feature de mail",
+          "Feature de changelog"
+        ]
+      },
+      {
+        version: 0.1,
+        date: new Date(),
+        bugs: [
+          "Correction de l'affichage déformé du header",
+          "Correction du flash lors du changement de prospect"
+        ],
+        features: [
+          "Feature de dashboard",
+          "Feature de mail",
+          "Feature de changelog"
+        ]
+      },
+      {
         version: 0.1,
         date: new Date(),
         bugs: [
