25dc662b9d9d1270c4b58f0bebfa0d17fcf5c907
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-09-04 21:50:26 +0200

    🩹 add admin guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,17 +8,18 @@ import { MeetingsComponent } from './components/meetings/meetings/meetings.compo
 import { RemindersComponent } from './components/reminders/reminders/reminders.component';
 import { ResearchProspectsComponent } from './components/research-prospects/research-prospects/research-prospects.component';
 import { UsersComponent } from './components/users/users/users.component';
+import { AdminGuard } from './guards/admin.guard';
 import { LoggedGuard } from './guards/logged.guard';
 import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent, children: [], canActivate: [LoginGuard] },
   { path:"dashboard", component: DashboardComponent, canActivate: [LoggedGuard] },
-  { path:"users", component: UsersComponent, canActivate: [LoggedGuard] },
+  { path:"users", component: UsersComponent, canActivate: [LoggedGuard, AdminGuard] },
   { path:"reminders", component: RemindersComponent, canActivate: [LoggedGuard] },
   { path:"meetings", component: MeetingsComponent, canActivate: [LoggedGuard] },
   { path:"prospects", component: ResearchProspectsComponent, canActivate: [LoggedGuard] },
-  { path:"goals", component: GoalsComponent, canActivate: [LoggedGuard] },
+  { path:"goals", component: GoalsComponent, canActivate: [LoggedGuard, AdminGuard] },
   { path: "bookmarks", component: BookmarksComponent, canActivate: [LoggedGuard] },
   { path:"", pathMatch: "full", redirectTo: "dashboard" },
   { path: "**", pathMatch: "full", redirectTo: "dashboard"},
