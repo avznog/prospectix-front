bc5f0e16d3251c134133bf04679d97b6eb4ff92b
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-11-27 17:45:28 +0100

    💥 add city feature

--- src/app/services/changelogs/changelogs.service.ts
+++ src/app/services/changelogs/changelogs.service.ts
@@ -12,6 +12,16 @@ export class ChangelogsService {
     this.localVersion = localStorage.getItem("version") ?? "";
     this.changelogs = [
       {
+        version: "1.5",
+        date: new Date("2022-11-27T00:00:00.000Z"),
+        bugs: [
+         
+          ],
+        features: [
+         "Possibilité d'ajouter une ville"
+        ],
+      },
+      {
         version: "1.4.1",
         date: new Date("2022-11-17T00:00:00.000Z"),
         bugs: [
