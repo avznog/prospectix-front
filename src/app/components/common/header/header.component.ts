1cea2acc9849cd96eab4145cdaf3982e19b784c0
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-12-07 19:05:53 +0100

    ✨ add christmas countdown

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -14,6 +14,7 @@ export class HeaderComponent implements OnInit {
 
   @Input("class") class = "";
   scrolling = false;
+  today: Date = new Date()
 
   constructor(
     public authService: AuthService,
