4143581fa068b519262fe2167e37703531d12961
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-12-21 16:34:27 +0100

    🚧 add edition mode to templates + deletion (not done)

--- src/app/components/mail-templates/mail-templates/mail-templates.component.ts
+++ src/app/components/mail-templates/mail-templates/mail-templates.component.ts
@@ -1,26 +1,31 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { MailTemplate } from 'src/app/models/mail-template.model';
 import { MailTemplatesService } from 'src/app/services/mail-templates/mail-templates.service';
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-mail-templates',
   templateUrl: './mail-templates.component.html',
-  styleUrls: ['./mail-templates.component.scss']
+  styleUrls: ['./mail-templates.component.scss'],
 })
 export class MailTemplatesComponent implements OnInit {
   editorConfig: AngularEditorConfig = {
     editable: true,
+    enableToolbar: true,
     height: '50vh',
     minHeight: '10vh',
     placeholder: 'Ecrivez votre mail ici, la forme sera conservée.',
     toolbarHiddenButtons: [
       ['undo', 'redo'],
       ['fontSize', 'insertImage', 'insertVideo']
-    ]
+    ],
+    
   };
   htmlContent: string = ""
   currentTemplate: MailTemplate | "creation" = "creation";
   name: string = ""
+  editMode: boolean = false;
   constructor(
     public readonly mailTemplatesService: MailTemplatesService
   ) { }
@@ -28,7 +33,8 @@ export class MailTemplatesComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  changeTemplate(mailTemplate: MailTemplate| "creation") {
+  changeTemplate(mailTemplate: MailTemplate | "creation") {
+    this.editMode = false;
     this.currentTemplate = mailTemplate;
   }
 
@@ -36,6 +42,17 @@ export class MailTemplatesComponent implements OnInit {
     this.mailTemplatesService.create({
       name: this.name,
       content: this.htmlContent
-    })
+    });
+   }
+
+   updateMailTemplate() {
+    this.currentTemplate != 'creation' && this.mailTemplatesService.update(this.currentTemplate.id, {
+      content: this.htmlContent,
+    }) && (this.currentTemplate.content = this.htmlContent);
+    
+   }
+
+   onClickDelete() {
+    this.currentTemplate != 'creation' && this.mailTemplatesService.delete(this.currentTemplate.id)
    }
 }
--- src/app/services/mail-templates/mail-templates.service.ts
+++ src/app/services/mail-templates/mail-templates.service.ts
@@ -4,6 +4,7 @@ import { MailTemplate } from 'src/app/models/mail-template.model';
 import { CreateMailTemplateDto } from 'src/app/dto/mail-templates/create-mail-template.dto';
 import { updateMailTemplateDto } from 'src/app/dto/mail-templates/update-mail-template.dto';
 import { ToastsService } from '../toasts/toasts.service';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,14 @@ import { ToastsService } from '../toasts/toasts.service';
 export class MailTemplatesService {
 
   myTemplates = new Map<number, MailTemplate>();
+  allTemplates = new Map<number, MailTemplate>();
   constructor(
     private http: HttpClient,
-    private readonly toastsService: ToastsService
+    private readonly toastsService: ToastsService,
+    private readonly authService: AuthService
   ) { 
     this.findAllForMe();
+    this.findAll();
   }
 
   findAllForMe() {
@@ -33,11 +37,33 @@ export class MailTemplatesService {
   }
 
   delete(id: number) {
-    return this.http.delete(`mail-templates/${id}`).subscribe(() => this.myTemplates.delete(id));
+    return this.http.delete(`mail-templates/${id}`).subscribe(() => {
+      const mailTemplateName = this.myTemplates.get(id)?.name;
+      this.myTemplates.delete(id)
+      console.log(this.myTemplates)
+      this.toastsService.addToast({
+        type: "alert-error",
+        message: `Mail Template ${mailTemplateName} supprimé`
+      })
+    });
   }
 
   update(id: number, updateMailTemplateDto: updateMailTemplateDto) {
-    return this.http.patch<MailTemplate>(`mail-templates/${id}`, updateMailTemplateDto).subscribe(mailTemplate => this.myTemplates.set(id, { ...this.myTemplates.get(id)!, ...updateMailTemplateDto}));
+    return this.http.patch<MailTemplate>(`mail-templates/${id}`, updateMailTemplateDto).subscribe(mailTemplate => {
+      this.myTemplates.set(id, mailTemplate)
+      console.log(mailTemplate)
+      this.toastsService.addToast({
+        type: "alert-info",
+        message: `Mail Template ${this.myTemplates.get(id)?.name} modifé`
+      });
+    });
+  }
+
+  findAll() {
+    return this.http.get<MailTemplate[]>(`mail-templates/all`).subscribe(mailTemplates => {
+      mailTemplates = mailTemplates.filter(mailtemplate => mailtemplate.pm.id != this.authService.currentUserSubject.getValue().id)
+      mailTemplates.forEach(mailTemplate => this.allTemplates.set(mailTemplate.id, mailTemplate));
+    })
   }
 
 }
