cfc261f9e706f559f7bdf7c88e5cdbc296b38322
Benjamin Gonzva <bgonzva@juniorisep.com>
2022-12-25 01:45:44 +0100

    🔖 version 3.0 changelogs revision

--- src/app/services/changelogs/changelogs.service.ts
+++ src/app/services/changelogs/changelogs.service.ts
@@ -20,13 +20,15 @@ export class ChangelogsService {
         features: [
           "Ajout de la possibilité de modifier un utilisateur",
           "Ajout d'une page 'Mon Compte'. Il est possible de visualiser ses informations et de se connecter / déconnecter / vérifier la connexion à Google",
-          "Ajout de l'implémentation de Google",
           "Ajout de l'implémentation des template de mails. Prospectix prend maintenant en charge l'envoi de mail",
+          "[MAILS]: Il est désomais possible de créer des templates de mails, dans une page réservée. Les administrateurs ont accès aux templates de tous les chefs de projets.",
+          "[MAILS]: L'envoi de mails se fait désormais avec les templates. Pour envoyer un mail, il faut choisir le template à envoyer, le sujet, et le nom du client. Il est possible de marquer un mail comme envoyé séparément.",
+          "[MAILS]: Le nom du template est désormais visible dans les mails envoyés et dans l'historique du prospect",
+          "Ajout de l'implémentation de Google",
           "[GOOGLE]: Lors de la création d'un rendez-vous par un chef de projet, le rendez-vous s'ajoute automatiquement dans l'agenda Google",
           "[GOOGLE]: Un rendez-vous créé envoie une notification au mail du client et au chef de projet. Si le rendez-vous est en distanciel, l'invitation génère un lien Google Meet",
-          "[MAILS]: Il est désomais possible de créer des templates de mails, dans une page réservée.",
-          "[MAILS]: L'envoi de mails se fait désormais avec les templates. Pour envoyer un mail, il faut choisir le template à envoyer, le sujet, et le nom du client. Il est possible de marquer un mail comme envoyé séparément.",
-          "[MAILS]: Le template est désormais visible dans les mails envoyés et dans l'historique du prospect"
+          "[GOOGLE]: Chaque chef de projet peut se connecter avec son compte JUNIORISEP depuis la page Mon Compte. Les tokens (gérés par Google) durent une semaine."
+          
         ]
       },
       {
