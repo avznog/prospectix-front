8d8b4b2253eb355e48ef2db1794b16e000c1eb7b
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-02-14 13:56:42 +0100

    💥 by dates watchtower

--- src/app/components/goals/watchtower/watchtower.component.ts
+++ src/app/components/goals/watchtower/watchtower.component.ts
@@ -10,15 +10,45 @@ import { StatisticsService } from 'src/app/services/statistics/statistics.servic
 })
 export class WatchtowerComponent implements OnInit {
 
+  dateDown: Date = new Date();
+  dateUp: Date = new Date();
   constructor(
     public readonly pmService: ProjectManagersService,
     public readonly statisticsService: StatisticsService,
-    public readonly goalTemplatesService: GoalTemplatesService  
+    public readonly goalTemplatesService: GoalTemplatesService
   ) { }
 
   ngOnInit(): void {
     this.statisticsService.countWeeklyAllCalls();
     this.statisticsService.countWeeklyAllMeetings();
+    this.dateDown = this.getMonday();
+    this.statisticsService.countAllCallsByWeeksForWatchtower({ dateDown: this.dateDown, dateUp: this.dateUp })
+    this.statisticsService.countAllMeetingsByWeeksForWatchtower({ dateDown: this.dateDown, dateUp: this.dateUp })
+  }
+
+  onClickCurrentWeek() {
+    this.dateDown = this.getMonday();
+    this.dateUp = new Date();
+    this.statisticsService.countAllCallsByWeeksForWatchtower({ dateDown: this.dateDown, dateUp: this.dateUp })
+    this.statisticsService.countAllMeetingsByWeeksForWatchtower({ dateDown: this.dateDown, dateUp: this.dateUp })
+  }
+
+  onChangeDate() {
+    this.statisticsService.countAllCallsByWeeksForWatchtower({dateDown: this.dateDown, dateUp: this.dateUp})
+    this.statisticsService.countAllMeetingsByWeeksForWatchtower({ dateDown: this.dateDown, dateUp: this.dateUp })
+
+  }
+
+  getMonday() {
+    const today = new Date();
+    //  ? getting the monday of the week
+    // const monday = new Date(today.setDate(firstd));
+    const date = new Date(today);
+    const day = date.getDay(); // 👉️ get day of week
+    // 👇️ day of month - day of week (-6 if Sunday), otherwise +1
+    const diff = date.getDate() - day + (day === 0 ? -6 : 1);
+
+    return new Date(date.setDate(diff));
   }
 
 }
