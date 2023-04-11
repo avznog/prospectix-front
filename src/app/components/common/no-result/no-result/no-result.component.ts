f77bf4bfa7a2f80af1034b63f592500a8bc3af44
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-04-11 17:47:30 +0200

    📱 update no result design

--- src/app/components/common/no-result/no-result/no-result.component.ts
+++ src/app/components/common/no-result/no-result/no-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-no-result',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./no-result.component.scss']
 })
 export class NoResultComponent implements OnInit {
-
-  constructor() { }
+  listErrorMessages : string[] = [
+    "Essaie d'autre catégories, tu trouveras surement mieux ...",
+    "Une autre ville possède renferme d'autres secrets ...",
+    "C'est en cherchant que le CDP trouva ...",
+    "Demande des prospects au pôle Techn... non, ils sont surbookés, change de catégories !",
+    "Recherche tous les prospects en A, certains te satisferont surement"
+  ];
+  currentErrorMessage : string = "Essaie d'autre catégories, tu trouveras surement mieux ...";
+  constructor(
+    public readonly authService: AuthService
+  ) { 
+    const randomIndex = Math.floor(Math.random() * this.listErrorMessages.length);
+    this.currentErrorMessage =  this.listErrorMessages[randomIndex];
+  }
 
   ngOnInit(): void {
+    const randomIndex = Math.floor(Math.random() * this.listErrorMessages.length);
+    this.currentErrorMessage =  this.listErrorMessages[randomIndex]; 
   }
 
 }
