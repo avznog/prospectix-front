515dc430ac229a6cbc2fd7c54fac240beb7577f2
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-04-27 09:59:15 +0200

    🔊 add loggin

--- src/app/services/prospects/prospects.service.ts
+++ src/app/services/prospects/prospects.service.ts
@@ -51,6 +51,7 @@ export class ProspectsService {
   }
 
   resetSearch(researchParamsProspect: ResearchParamsProspect) {
+    console.log(researchParamsProspect)
     this.loading = true;
     this.prospects.clear();
     this.updateSearchParameters({
@@ -67,7 +68,6 @@ export class ProspectsService {
   }
 
   loadMore() {
-    console.time();
     let queryParameters = new HttpParams();
     queryParameters = queryParameters.append("take", 20);
     this.researchParamsProspect.skip && (queryParameters = queryParameters.append("skip", this.researchParamsProspect.skip));
@@ -80,7 +80,6 @@ export class ProspectsService {
       data.prospects.forEach(prospect => this.prospects.set(prospect.id, prospect));
       this.nbProspects = data.count;
       this.loading = false;
-      console.timeEnd()
     });
   }
 
